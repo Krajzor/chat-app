efd86f4acb9d107679003b9fe740c4eedd0bc783
Krajzor <mobiusff1982@gmail.com>
2025-12-20 17:57:36 +0100

    Created the contact section and added html file and css styles.

---
+++ contact-info.js
@@ -0,0 +1,18 @@
+export const contactInfo = [{
+    id: 1,
+    name: 'Uciekinier Polski',
+    pfp: 'gonitramwaj.jpg',
+    phoneNumber: 123456789
+},
+{
+    id: 2,
+    name: 'Przeciwnik Oklahomy',
+    pfp: 'Parleiq.png',
+    phoneNumber: 222333444
+},
+{
+    id: 3,
+    name: 'Shadow Wizard Money Gang',
+    pfp: 'goldcoin.png',
+    phoneNumber: 420420420
+}];
