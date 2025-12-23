3bc19168d916362d362b16aeea3eb912deb41f38
Krajzor <mobiusff1982@gmail.com>
2025-12-23 11:21:29 +0100

    Make chat messages generate

--- contact-info.js
+++ contact-info.js
@@ -16,3 +16,60 @@ export const contactInfo = [{
     pfp: 'goldcoin.png',
     phoneNumber: 420420420
 }];
+
+
+export const messages = [{
+    id: 1,
+    content: `Hows it going bro?`,
+    sentOrReceived: 'sent'
+},
+{
+    id: 2,
+    content: `Good.`,
+    sentOrReceived: 'received'
+},
+{
+    id: 3,
+    content: `That 's cool.`,
+    sentOrReceived: 'sent'
+},
+{
+    id: 4,
+    content: '💀',
+    sentOrReceived: 'received'
+},
+{
+    id: 5,
+    content: '💀',
+    sentOrReceived: 'received'
+},
+{
+    id: 6,
+    content: '💀',
+    sentOrReceived: 'received'
+},
+{
+    id: 7,
+    content: '💀',
+    sentOrReceived: 'received'
+},
+{
+    id: 8,
+    content: '💀',
+    sentOrReceived: 'received'
+},
+{
+    id: 9,
+    content: '💀',
+    sentOrReceived: 'received'
+},
+{
+    id: 10,
+    content: '💀',
+    sentOrReceived: 'received'
+},
+{
+    id: 11,
+    content: '💀',
+    sentOrReceived: 'received'
+}]
