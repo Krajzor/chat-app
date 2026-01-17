56c94f7a21079cc93569e93cc26b078a4de5709a
Krajzor <mobiusff1982@gmail.com>
2026-01-17 16:16:49 +0100

    Fixed routes

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use('/api/messages', messageRoutes);
 
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
